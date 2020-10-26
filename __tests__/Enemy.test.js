f43eabe70e6733b4ebc98876146b21e030e75aa0
Jonny Lightheart <jonnylightheart@gmail.com>
2020-10-26 16:39:31 -0600

    added inquirer

--- __tests__/Enemy.test.js
+++ __tests__/Enemy.test.js
@@ -57,4 +57,4 @@ test('gets a description of the enemy', () => {
 
     expect(enemy.getDescription()).toEqual(expect.stringContaining('goblin'));
     expect(enemy.getDescription()).toEqual(expect.stringContaining('sword'));
-})
+});
